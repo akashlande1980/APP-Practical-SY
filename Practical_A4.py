46cd1fb50f8cdb80c5785d9c11413c4a1a609e5b
akashlande1980 <akashlande1980@gmail.com>
2026-08-23 20:24:30 -0700

    Update Practical_A4.py

--- Practical_A4.py
+++ Practical_A4.py
@@ -1,13 +1,14 @@
-n=int(input("enter the value of n: "))
-if n==0:
-    print("fibonacci number is:" ,0)
-elif n==1:
-    print("fibonacci number is:" ,1)
-else:
-    a=0
-    b=1
-    for i in range(2,n+1):
-        c=a+b
-        a=b
-        b=c
-        print("fibonacci number is:",b)
+  if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+    return b
+n = int(input("Enter the value of n: "))
+result = fibonacci_iterative(n)
+print("Fibonacci number is:", result)
+  
